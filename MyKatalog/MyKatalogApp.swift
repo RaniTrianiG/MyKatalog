488533b24cfc7982d3c57facb38a63a89b471a6f
RaniTrianiG <ranitrianig@gmail.com>
2021-10-19 01:02:14 +0700

    Initial Commit

---
+++ MyKatalog/MyKatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyKatalogApp.swift
+//  MyKatalog
+//
+//  Created by Rani Triani Gustia on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyKatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
